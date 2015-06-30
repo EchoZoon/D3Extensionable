74cd1168e8ce32fe9e0c709ddaf3e90bb628a7a3
IChocolateKapa <echochocolatekapa@163.com>
2015-06-30 09:34:25 +0800

    Update getQQ_QunInfo.java

--- src/getQQ_QunInfo/getQQ_QunInfo.java
+++ src/getQQ_QunInfo/getQQ_QunInfo.java
@@ -2,59 +2,10 @@ package getQQ_QunInfo;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import common.toolUtils;
-
 import dataStructsUtils.QQResult;
 import dataStructsUtils.QunResult;
-import dataStructsUtils.semaName;
-import entity.Results;
-import entity.RowEntity;
 
 public class getQQ_QunInfo {
-	
-	/**
-	 * get a qq's info
-	 * @param results
-	 * @return
-	 */
-	public static QQResult getLeftQQResult(RowEntity results) {
-		
-		QQResult qqResult = new QQResult();
-
-		String qqAccount = results.getByName(semaName.qqAccount).getValue();
-		qqResult.setQqAccount(qqAccount);
-		
-		String qqNickName = results.getByName(semaName.qqNickName) == null ? "" : results.getByName(semaName.qqNickName).getValue();
-//		System.out.println(qqNickName);
-		qqResult.setQqNickName(qqNickName);
-		
-		String qqName = results.getByName(semaName.qqName) == null ? "" : results.getByName(semaName.qqName).getValue();
-		qqResult.setQqName(qqName);
-		
-		String qqSex = results.getByName(semaName.qqSex) == null ? "" : results.getByName(semaName.qqSex).getValue();
-//		System.out.println(qqSex);
-		String qqSex1 = toolUtils.getSexString(qqSex);
-		qqResult.setQqSex(qqSex1);
-		
-		String qqAge = results.getByName(semaName.qqAge) == null ? "" : results.getByName(semaName.qqAge).getValue();
-		qqResult.setQqAge(qqAge);
-		
-		String qunNumber = results.getByName(semaName.qunNumber) == null ? "" : results.getByName(semaName.qunNumber).getValue();
-		qqResult.setQunNumber(qunNumber);
-		
-		String qunNickName = results.getByName(semaName.qunNickName) == null ? "" : results.getByName(semaName.qunNickName).getValue();
-		qqResult.setQunNickName(qunNickName);
-//		qqResult.setQunNickName("假数据喔");
-		
-		String qunDescription = results.getByName(semaName.qunDescription) == null ? "" : results.getByName(semaName.qunDescription).getValue();
-		qqResult.setQunDescription(qunDescription);
-//		qqResult.setQunDescription("由于qun_info数据没有入完，所以这里是测试数据");
-		
-		return qqResult;
-		
-	}
-	
 
 	
 	public static QQResult getFakeQQResult() {
